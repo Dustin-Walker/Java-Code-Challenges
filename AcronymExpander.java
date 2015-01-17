e4cdd783446c8b941a233f50c4d3dba336c6213e
Dustin-Walker <duwalker88@gmail.com>
2015-01-16 20:06:54 -0500

    Initial commit

---
+++ AcronymExpander.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+
+public class AcronymExpander {
+
+    public static void main(String[] args) {
+        AcronymExpander ae = new AcronymExpander();
+        ae.preloadAcronymList();
+        System.out.println(ae.expander("imo that was wp. Anyway I've g2g"));
+    }
+
+    private HashMap<String, String> acronymList = new HashMap<String, String>();
+
+    public void preloadAcronymList(){
+        acronymList.put("lol", "laugh out loud");
+        acronymList.put("dw", "don't worry");
+        acronymList.put("hf", "have fun");
+        acronymList.put("gg", "good game");
+        acronymList.put("brb", "be right back");
+        acronymList.put("g2g", "got to go");
+        acronymList.put("wtf", "what the fuck");
+        acronymList.put("wp", "well played");
+        acronymList.put("gl", "good luck");
+        acronymList.put("imo", "in my opinion");
+    };
+
+    /**
+     * Replaces acronyms in the string with expanded words.
+     * @param s String to be modified
+     * @return Modified string with acronyms replaced with full words
+     */
+    public String expander(String s){
+        for(String ls : acronymList.keySet())
+            if(s.contains(ls))
+                s = s.replaceAll("\\b"+ls+"\\b", acronymList.get(ls));
+        return s;
+    }
+}
