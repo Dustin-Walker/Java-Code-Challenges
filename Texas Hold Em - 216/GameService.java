6cf7d8ea2de63832f4d4449662d0d0f12b24eee0
Dustin Walker <duwalker88@gmail.com>
2015-06-01 19:54:27 -0400

    Finished the easy level challenge for Texas Hold Em.

--- Texas Hold Em - 216/GameService.java
+++ Texas Hold Em - 216/GameService.java
@@ -9,15 +9,32 @@ public class GameService {
 
     public static void main(String[] args) {
         Deck deck = new Deck();
+        GameService gameService = new GameService();
         //for (Card card : deck)
           //  System.out.println(card.toString());
-        int numberOfPlayers = new GameService().queryNumberOfPlayers();
-        HashMap<Integer, Player> playerList = new GameService().dealNewGame(numberOfPlayers, deck);
-     //   for (Player player : playerList.values())
-    //        System.out.println("Player has "+player.hand[0]+" and "+player.hand[1]);
+        int numberOfPlayers = gameService.queryNumberOfPlayers();
+        System.out.println();
+        HashMap<Integer, Player> playerList = gameService.dealNewCardsToPlayers(numberOfPlayers, deck);
+        gameService.flop(deck);
+        for (Player player : playerList.values())
+            System.out.println("Player has "+player.hand[0]+" and "+player.hand[1]);
+        System.out.println("\n"+SharedCards.flopToString());
+        gameService.turn(deck);
+        System.out.println(SharedCards.turnToString());
+        gameService.river(deck);
+        System.out.println(SharedCards.riverToString());
     }
 
-    public int queryNumberOfPlayers(){
+    private void turn(Deck deck) {
+        deck.pop();
+        SharedCards.add(deck.pop());
+    }
+
+    private void river(Deck deck){
+        turn(deck);
+    }
+
+    private int queryNumberOfPlayers(){
         System.out.println("How many players (2-8) ? ");
         Scanner sc = new Scanner(System.in);
         return sc.nextInt();
@@ -28,13 +45,24 @@ public class GameService {
      * @param numberOfPlayers The number of players in the game
      * @param deck The deck of cards to deal from
      */
-    public HashMap<Integer, Player> dealNewGame(int numberOfPlayers, Deck deck){
+    private HashMap<Integer, Player> dealNewCardsToPlayers(int numberOfPlayers, Deck deck){
         assert numberOfPlayers <=8 && numberOfPlayers >= 2;
         HashMap<Integer, Player> playerSet = new HashMap<Integer, Player>(numberOfPlayers);
         for (int playerIndex = 0; playerIndex < numberOfPlayers; playerIndex++) {
             playerSet.put(playerIndex, new Player(deck.pop(), deck.pop()));
         }
         return playerSet;
+    }
+
+    /**
+     * pop pop pop
+     * @param deck Deck to work with
+     */
+    private void flop(Deck deck){
+        deck.pop();
+        SharedCards.add(deck.pop());
+        SharedCards.add(deck.pop());
+        SharedCards.add(deck.pop());
     }
 
 }
