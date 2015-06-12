c5178a35b9cbbc05a1c5f1841ecc6c5dc203dbbd
Dustin Walker <duwalker88@gmail.com>
2015-06-12 16:38:08 -0400

    Optimized the high card method.

--- Texas Hold Em - 216/CardType.java
+++ Texas Hold Em - 216/CardType.java
@@ -1,4 +1,4 @@
 /**
  * Created by user on 5/31/15.
  */
-public enum CardType { Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King }
+public enum CardType { Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace }
