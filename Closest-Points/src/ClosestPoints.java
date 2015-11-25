268875ec9c2aa10d7082b1325d0b9a0578ad1449
Dustin Walker <duwalker88@gmail.com>
2015-11-25 17:06:38 -0500

    Updated the Closest Points challenge. Working on an optimized solution to the problem.

--- Closest-Points/src/ClosestPoints.java
+++ Closest-Points/src/ClosestPoints.java
@@ -1,8 +1,10 @@
+import java.awt.*;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.*;
 import java.util.List;
 
 public class ClosestPoints {
@@ -33,6 +35,12 @@ public class ClosestPoints {
         return Files.readAllLines(path);
     }
 
+    /**
+     * Brute force approach
+     * Runs in O(n^2) time
+     * @param list List of points
+     * @return String array representing points
+     */
     public String[] findClosestPoints(List<String> list){
         int length = list.size();
         for (int i = 1; i < length-1; i++) {
@@ -63,6 +71,93 @@ public class ClosestPoints {
             s = s.substring(0, s.length()-1);
         }
         return s;
+    }
+
+    /**
+     * Optimized solution to the closest points problem.
+     * Uses a more object oriented approach.
+     * Runs in O(n log n) time.
+     * Based on the algorithm described in Introduction to Algorithms
+     *  3rd Edition by Cormen, et al on page 1039.
+     * This is a divide and conquer based approach.
+     * @param P Subset of all points
+     * @param X Set P ordered by X coordinate
+     * @param Y Set P ordered by Y coordinate
+     * @return Closest two points
+     */
+    public Point[] getClosestPoints(Set<Point> P, List<Point> X, List<Point> Y){
+        // Brute force case
+        if (P.size() <= 3){
+            // brute force solution
+        }
+        // Divide step involves dividing and sorting points into groups
+        int sumX = 0, sumY = 0;
+        Set<Point> PL = new HashSet<>();
+        Set<Point> PR = new HashSet<>();
+        for (Point point : P){
+            sumX += point.getX();
+            sumY += point.getY();
+        }
+        // Divide points into two equal sized groups on either side of a vertical line
+        Set<Point> XL = new TreeSet<Point>(new Comparator<Point>() {
+            @Override
+            public int compare(Point o1, Point o2) {
+                double x1 = o1.getX(), x2 = o2.getX();
+                if (x1 < x2)
+                    return -1;
+                if (x1 > x2)
+                    return 1;
+                return 0;
+            }
+        });
+        Set<Point> XR = new TreeSet<Point>(new Comparator<Point>() {
+            @Override
+            public int compare(Point o1, Point o2) {
+                double x1 = o1.getX(), x2 = o2.getX();
+                if (x1 < x2)
+                    return -1;
+                if (x1 > x2)
+                    return 1;
+                return 0;
+            }
+        });
+
+        // Divide points into two equal sized groups on either side of a horizontal line
+        Set<Point> YL = new TreeSet<Point>(new Comparator<Point>() {
+            @Override
+            public int compare(Point o1, Point o2) {
+                double y1 = o1.getY(), y2 = o2.getY();
+                if (y1 < y2)
+                    return -1;
+                if (y1 > y2)
+                    return 1;
+                return 0;
+            }
+        });
+        Set<Point> YR = new TreeSet<Point>(new Comparator<Point>() {
+            @Override
+            public int compare(Point o1, Point o2) {
+                double y1 = o1.getY(), y2 = o2.getY();
+                if (y1 < y2)
+                    return -1;
+                if (y1 > y2)
+                    return 1;
+                return 0;
+            }
+        });
+
+        for (Point point : P){
+            if (point.getX() < sumX / 2)
+                PL.add(point);
+            else
+                PR.add(point);
+        }
+
+        XL.addAll(PL);
+        XR.addAll(PR);
+        YL.addAll(PL);
+        YR.addAll(PR);
+
     }
 
 
